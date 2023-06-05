007d04f4e1a944066722fbad36d18ad1968a5f67
MrMojoRising777 <alexander.goyens@gmail.com>
2023-06-05 10:53:18 +0200

    draggable marker + console.log live coordinates

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -16,41 +16,55 @@
       <script>
           // Map initialization code
           var map = new ol.Map({
-                target: 'map',
-                layers: [
-                    new ol.layer.Tile({
-                        source: new ol.source.OSM()
-                    })
-                ],
-                view: new ol.View({
-                    center: ol.proj.fromLonLat([5.536490, 50.994720]), // Genk coordinates: [longitude, latitude]
-                    zoom: 13 // Adjust the zoom level as needed
-                })
-            });
+              target: 'map',
+              layers: [
+                  new ol.layer.Tile({
+                      source: new ol.source.OSM()
+                  })
+              ],
+              view: new ol.View({
+                  center: ol.proj.fromLonLat([5.536490, 50.994720]), // Genk coordinates: [longitude, latitude]
+                  zoom: 13 // Adjust the zoom level as needed
+              })
+          });
 
-            // Create a marker overlay
-            var marker = new ol.Feature({
-                geometry: new ol.geom.Point(ol.proj.fromLonLat([5.536490, 50.994720])) // Marker coordinates: [longitude, latitude]
-            });
+          // Create a marker overlay
+          var marker = new ol.Feature({
+              geometry: new ol.geom.Point(ol.proj.fromLonLat([5.536490, 50.994720])) // Marker coordinates: [longitude, latitude]
+          });
 
-            var markerStyle = new ol.style.Style({
-                image: new ol.style.Icon({
-                    anchor: [0.5, 1], // Set the anchor point of the marker icon
-                    src: 'https://openlayers.org/en/latest/examples/data/icon.png' // URL to the marker icon image
-                })
-            });
+          var markerStyle = new ol.style.Style({
+              image: new ol.style.Icon({
+                  anchor: [0.5, 1], // Set the anchor point of the marker icon
+                  src: 'https://openlayers.org/en/latest/examples/data/icon.png' // URL to the marker icon image
+              })
+          });
 
-            marker.setStyle(markerStyle);
+          marker.setStyle(markerStyle);
 
-            var vectorSource = new ol.source.Vector({
-                features: [marker]
-            });
+          var vectorSource = new ol.source.Vector({
+              features: [marker]
+          });
 
-            var vectorLayer = new ol.layer.Vector({
-                source: vectorSource
-            });
+          var vectorLayer = new ol.layer.Vector({
+              source: vectorSource
+          });
 
-            map.addLayer(vectorLayer);
+          map.addLayer(vectorLayer);
+
+          // Make the marker draggable
+          var dragInteraction = new ol.interaction.Translate({
+              features: new ol.Collection([marker])
+          });
+
+          map.addInteraction(dragInteraction);
+
+          // Display the latitude and longitude when the marker is dragged
+          marker.on('change', function () {
+              var coordinates = marker.getGeometry().getCoordinates();
+              var lonLat = ol.proj.toLonLat(coordinates);
+              console.log('Latitude: ' + lonLat[1] + ', Longitude: ' + lonLat[0]);
+          });
       </script>
   </body>
 </html>
