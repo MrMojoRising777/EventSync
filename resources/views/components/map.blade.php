8e3fa91e3c83013dcb1460fb9d38bae496ee9de8
MrMojoRising777 <alexander.goyens@gmail.com>
2023-06-05 11:01:39 +0200

    change user input: street - zipcode - city

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -8,11 +8,44 @@
         width: 100%;
         height: 400px;
       }
+      .input-container {
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+      }
+      .input-container label {
+        margin-right: 10px;
+        font-weight: bold;
+      }
+      .input-container input {
+        padding: 5px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+      }
+      .input-container button {
+        padding: 5px 10px;
+        background-color: #4caf50;
+        color: #fff;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+      }
     </style>
   </head>
   <body>
-    <div>
-      <input id="addressInput" type="text" placeholder="Enter an address" />
+    <div class="input-container">
+      <label for="streetInput">Street:</label>
+      <input id="streetInput" type="text" placeholder="Enter a street name" />
+    </div>
+    <div class="input-container">
+      <label for="zipInput">Zip Code:</label>
+      <input id="zipInput" type="text" placeholder="Enter a zip code" />
+    </div>
+    <div class="input-container">
+      <label for="cityInput">City:</label>
+      <input id="cityInput" type="text" placeholder="Enter a city" />
+    </div>
+    <div class="input-container">
       <button onclick="geocodeAddress()">Search</button>
     </div>
     <div id="map"></div>
@@ -63,36 +96,38 @@
       }
 
       function geocodeAddress() {
-        var address = document.getElementById('addressInput').value;
+        var street = document.getElementById('streetInput').value;
+        var zip = document.getElementById('zipInput').value;
+        var city = document.getElementById('cityInput').value;
+        var address = street + ', ' + zip + ' ' + city + ', Belgium';
+
         var url =
-          'https://nominatim.openstreetmap.org/search?format=json&q=' +
-          encodeURIComponent(address) +
-          '&countrycodes=be&limit=1';
+            'https://nominatim.openstreetmap.org/search?format=json&q=' +
+            encodeURIComponent(address) +
+            '&countrycodes=be&limit=1';
 
-        fetch(url)
-          .then(function(response) {
+        fetch(url).then(function(response) {
             return response.json();
-          })
-          .then(function(data) {
-            if (data.length > 0) {
-              var lat = parseFloat(data[0].lat);
-              var lon = parseFloat(data[0].lon);
-              var coordinate = ol.proj.fromLonLat([lon, lat]);
+        })
+        .then(function(data) {
+          if (data.length > 0) {
+            var lat = parseFloat(data[0].lat);
+            var lon = parseFloat(data[0].lon);
+            var coordinate = ol.proj.fromLonLat([lon, lat]);
 
-              marker.setGeometry(new ol.geom.Point(coordinate));
+            marker.setGeometry(new ol.geom.Point(coordinate));
 
-              map.getView().setCenter(coordinate);
-              map.getView().setZoom(15);
-            } else {
-              alert('Address not found');
-            }
-          })
-          .catch(function(error) {
-            console.log('Error:', error);
-          });
+            map.getView().setCenter(coordinate);
+            map.getView().setZoom(15);
+          } else {
+                alert('Address not found');
+          }
+        })
+        .catch(function(error) {
+          console.log('Error:', error);
+        });
       }
-
-      document.addEventListener('DOMContentLoaded', function() {
+        document.addEventListener('DOMContentLoaded', function() {
         initMap();
       });
     </script>
