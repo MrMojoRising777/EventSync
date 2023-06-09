15c1e7e366691f07f12d2095260c7a70075f8097
MrMojoRising777 <alexander.goyens@gmail.com>
2023-06-09 09:44:50 +0200

    map - minor tweak

--- resources/views/components/map.blade.php
+++ resources/views/components/map.blade.php
@@ -64,92 +64,93 @@
   </head>
   <body>
     <!-- header -->
-  <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-    <div class="container">
-      <a class="navbar-brand" href="{{ url('/') }}">
-        {{ config('app.name', 'Laravel') }}
-      </a>
-                <a class="navbar-brand" href="{{ url('/map') }}">
-                    {{ config('Map', 'Map') }}
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+      <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+          {{ config('app.name', 'Laravel') }}
+        </a>
+        <a class="navbar-brand" href="{{ url('/map') }}">
+          {{ config('Map', 'Map') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav me-auto">
+
+          </ul>
+
+          <!-- Right Side Of Navbar -->
+          <ul class="navbar-nav ms-auto">
+            <!-- Authentication Links -->
+            @guest
+              @if (Route::has('login'))
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+              @endif
+
+              @if (Route::has('register'))
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+              @endif
+
+              @else
+              <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  {{ Auth::user()->name }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                  </a>
 
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                  </form>
                 </div>
-            </div>
-        </nav>
-
-        <div class="card">
-          <div class="card-body">
-            <form>
-              <div class="form-row">
-                <div class="col-md-4 mb-3">
-                  <label for="streetInput">Straat:</label>
-                  <input id="streetInput" class="form-control" type="text" placeholder="Enter a street name" />
-                </div>
-                <div class="col-md-4 mb-3">
-                  <label for="zipInput">Postcode:</label>
-                  <input id="zipInput" class="form-control" type="text" placeholder="Enter a zip code" />
-                </div>
-                <div class="col-md-4 mb-3">
-                  <label for="cityInput">Stad:</label>
-                  <input id="cityInput" class="form-control" type="text" placeholder="Enter a city" />
-                </div>
-              </div>
-              <div class="form-group">
-                <button class="btn btn-primary" onclick="geocodeAddress()">Search</button>
-              </div>
-            </form>
-          </div>
+              </li>
+            @endguest
+          </ul>
         </div>
+      </div>
+    </nav>
 
-        <div id="map" style="height: 400px;"></div>
+    <div class="card">
+      <div class="card-body">
+        <form>
+          <div class="form-row">
+            <div class="col-md-4 mb-3">
+              <label for="streetInput">Straat:</label>
+              <input id="streetInput" class="form-control" type="text" placeholder="Enter a street name" />
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="zipInput">Postcode:</label>
+              <input id="zipInput" class="form-control" type="text" placeholder="Enter a zip code" />
+            </div>
+            <div class="col-md-4 mb-3">
+              <label for="cityInput">Stad:</label>
+              <input id="cityInput" class="form-control" type="text" placeholder="Enter a city" />
+            </div>
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary" onclick="geocodeAddress()">Search</button>
+          </div>
+        </form>
+      </div>
+    </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/ol/dist/ol.js"></script>
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
+    <div id="map" style="height: 400px;"></div>
+
+    <script src="https://cdn.jsdelivr.net/npm/ol/dist/ol.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
 
     <script>
       var map;
@@ -210,7 +211,6 @@
         map.addLayer(vectorLayer);
       }
 
-      // enable predicting street
       $(document).ready(function() {
         $('#streetInput').autocomplete({
           source: function(request, response) {
@@ -222,9 +222,10 @@
               },
               dataType: 'json',
               data: {
-                q: request.term + ' straat, België', // Include "straat" (street) in the search query
+                q: request.term,
                 format: 'json',
                 addressdetails: 1,
+                countrycodes: 'be', // Limit the search to Belgium
                 limit: 5
               },
               success: function(data) {
