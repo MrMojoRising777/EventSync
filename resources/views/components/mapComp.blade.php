42bbe708b9a139f3b765d11a497bd565894aca6f
MrMojoRising777 <alexander.goyens@gmail.com>
2023-06-09 20:58:41 +0200

    Fixed issue w/ calendar not being shown on /home

--- resources/views/components/mapComp.blade.php
+++ resources/views/components/mapComp.blade.php
@@ -8,9 +8,6 @@
 <!-- JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/ol/dist/ol.js"></script>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://code.jquery.com/ui/1.13.0/jquery-ui.js"></script>
-
 <script>
   // Initialize map
   var map;
