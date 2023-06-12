618bccb0300b990337862ccb59e4aa6612b4bae1
MrMojoRising777 <alexander.goyens@gmail.com>
2023-06-12 14:21:22 +0200

    CreateEventPage - style changes

--- resources/views/createEvent.blade.php
+++ resources/views/createEvent.blade.php
@@ -13,19 +13,49 @@
 
       <!-- Calendar Component -->
       <div class="form-group">
-        <label for="event_date">Event Date</label>
-        @include('components.calendar')
+        <div class="card">
+          <div class="card-header">{{ __('Date') }}</div>
+          <div class="card-body">
+            <label for="event_date">Event Date</label>
+            <div class="calendar-container">
+              @include('components.calendar')
+            </div>
+          </div>
+        </div>
       </div>
 
       <!-- Map Component -->
       <div class="form-group">
-        <label for="event_location">Event Location</label>
-        @include('components.mapComp')
+        <div class="card">
+          <div class="card-header">{{ __('Map') }}</div>
+          <div class="card-body">
+            <form>
+              <div class="row">
+                <div class="col-md-4 mb-3">
+                  <label for="streetInput">Straat:</label>
+                  <input id="streetInput" class="form-control" type="text" placeholder="Geef een straatnaam">
+                </div>
+                <div class="col-md-4 mb-3">
+                  <label for="zipInput">Postcode:</label>
+                  <input id="zipInput" class="form-control" type="text" placeholder="Geef een postcode">
+                </div>
+                <div class="col-md-4 mb-3">
+                  <label for="cityInput">Stad:</label>
+                  <input id="cityInput" class="form-control" type="text" placeholder="Geef een stad">
+                </div>
+              </div>
+              <div class="form-group">
+                <button class="btn btn-primary" onclick="geocodeAddress()">Zoek</button>
+              </div>
+            </form>
+          @include('components.mapComp')
+          </div>
+        </div>
       </div>
 
       <div id="friends-container" class="friends-container">
         <div class="card friends-container">
-          <div class="card-header">{{ __('Vrienden') }}</div>
+          <div class="card-header">{{ __('Friends') }}</div>
           <div class="card-body">
             <ul>
               <li>friend 1</li>
