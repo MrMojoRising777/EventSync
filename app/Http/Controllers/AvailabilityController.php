cde68b8e106d88b71266adf9cecf91a1c54e73b5
Yason Prüfer <yason.pr@gmail.com>
2023-06-20 10:42:58 +0200

    ability to have multiple start and end dates

--- app/Http/Controllers/AvailabilityController.php
+++ app/Http/Controllers/AvailabilityController.php
@@ -25,29 +25,45 @@ class AvailabilityController extends Controller
 
 
     public function store(Request $request)
-    {
-        $validatedData = $request->validate([
-            'event_id' => 'required',
-            'start_date' => 'required|date',
-            'end_date' => 'required|date|after_or_equal:start_date',
-        ]);
+{
+    $request->validate([
+        'event_id' => 'required',
+        'start_date' => 'required|array',
+        'end_date' => 'required|array',
+    ]);
 
-        $validatedData['user_id'] = Auth::id();
+    $event_id = $request->event_id;
+    $start_dates = $request->start_date;
+    $end_dates = $request->end_date;
+    $user_id = Auth::id();
 
-        $existingAvailability = Availability::where('user_id', $validatedData['user_id'])
-            ->where('event_id', $validatedData['event_id'])
-            ->first();
+    for ($i = 0; $i < count($start_dates); $i++) {
+        // Check if a valid end_date for a given start_date exists
+        if (isset($end_dates[$i]) && $end_dates[$i] >= $start_dates[$i]) {
+            $existingAvailability = Availability::where('user_id', $user_id)
+                ->where('event_id', $event_id)
+                ->whereBetween('start_date', [$start_dates[$i], $end_dates[$i]])
+                ->orWhereBetween('end_date', [$start_dates[$i], $end_dates[$i]])
+                ->first();
 
-        if ($existingAvailability) {
-            return redirect()->route('availabilities.index')
-                ->with('error', 'You have already created an availability for this event.');
+            if ($existingAvailability) {
+                return redirect()->route('availabilities.index')
+                    ->with('error', 'You have already created an availability for this event in the specified time range.');
+            }
+
+            $availability = new Availability;
+            $availability->user_id = $user_id;
+            $availability->event_id = $event_id;
+            $availability->start_date = $start_dates[$i];
+            $availability->end_date = $end_dates[$i];
+            $availability->save();
         }
-
-        Availability::create($validatedData);
-
-        return redirect()->route('availabilities.index')
-            ->with('success', 'Availability created successfully.');
     }
+
+    return redirect()->route('availabilities.index')
+        ->with('success', 'Availabilities created successfully.');
+}
+
 
 
 
--- resources/views/availabilities/create.blade.php
+++ resources/views/availabilities/create.blade.php
@@ -18,12 +18,46 @@
             </select>
         @endif
 
-        <label>Start Date:</label>
-        <input type="date" name="start_date">
+        <div id="date-ranges">
+            <div class="date-range">
+                <label>Start Date:</label>
+                <input type="date" name="start_date[]">
 
-        <label>End Date:</label>
-        <input type="date" name="end_date">
+                <label>End Date:</label>
+                <input type="date" name="end_date[]">
+            </div>
+        </div>
+
+        <button type="button" id="add-date-range">Add another date range</button>
 
         <button type="submit">Add</button>
     </form>
+    
+    <script>
+        document.getElementById('add-date-range').addEventListener('click', function() {
+            var dateRangeContainer = document.getElementById('date-ranges');
+            var newDateRange = document.createElement('div');
+            newDateRange.className = 'date-range';
+
+            var newStartDateLabel = document.createElement('label');
+            newStartDateLabel.textContent = 'Start Date:';
+            newDateRange.appendChild(newStartDateLabel);
+
+            var newStartDateInput = document.createElement('input');
+            newStartDateInput.type = 'date';
+            newStartDateInput.name = 'start_date[]';
+            newDateRange.appendChild(newStartDateInput);
+
+            var newEndDateLabel = document.createElement('label');
+            newEndDateLabel.textContent = 'End Date:';
+            newDateRange.appendChild(newEndDateLabel);
+
+            var newEndDateInput = document.createElement('input');
+            newEndDateInput.type = 'date';
+            newEndDateInput.name = 'end_date[]';
+            newDateRange.appendChild(newEndDateInput);
+
+            dateRangeContainer.appendChild(newDateRange);
+        });
+    </script>
 @endsection
