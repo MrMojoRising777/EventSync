8cd21b9e41ce694f383e87be87bda9dacecc5edc
Kevin98 <kevinjespers98@gmail.com>
2023-06-20 10:30:23 +0200

    only display events that are yet to happen

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\HomeController as  HomeController;
 use App\Models\Event;
 use App\Models\User;
+use Carbon\Carbon;
 
 class EventController extends Controller
 {
@@ -13,8 +14,14 @@ class EventController extends Controller
     {   
         
         $user = auth()->user(); 
-        $events = $user->events()->orderBy('date', 'asc')->paginate(5);
+        // $events = $user->events()->orderBy('date', 'asc')->paginate(5);
 
+        $currentDate = Carbon::today();
+
+        $events = $user->events()
+            ->whereDate('date', '>=', $currentDate)
+            ->orderBy('date', 'asc')
+            ->paginate(10);
 
         //$events = $own->events()->orderBy('date', 'asc')->cursorPaginate(1);
         
