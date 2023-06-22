71650f89afeb89cb63dbb2cedd90d28669780aae
MrMojoRising777 <alexander.goyens@gmail.com>
2023-06-22 20:25:06 +0200

    /events styling

--- resources/views/events/eventsDisplay.blade.php
+++ resources/views/events/eventsDisplay.blade.php
@@ -2,31 +2,30 @@
 
 @section('content')
 <div class="container">
+  <h2>Your Events</h2>
   @if ($ownedEvents->isEmpty())
-  <h2>Your Events</h2>
-  <p><i><b>Currently you aren't part of any events. Don't wait on friends, create your own and invite them!</b></i></p>
+    <p><i><b>Currently you don't have any organized events. Invite some friends, Sync up and create memories!</b></i></p>
   @else
-  <h2>Your Events</h2>
-  @foreach ($ownedEvents as $ownedevent)
-  <div class="card">
-    <div class="card-header">
-      {{$ownedevent->name}}
-    </div>
-    <div class="container text-center">
-      <div class="row justify-content-start">
-        <div class="col-4">
-          {{-- {{$ownedevent->location}} --}}
-        </div>
-        <div class="col-4">
-          {{$ownedevent->date}}
-        </div>
-        <div class="col-4">
-          <a href="{{route('Event', ['id' => $ownedevent->id])}}">info</a>
+    @foreach ($ownedEvents as $ownedevent)
+    <div class="card mb-3">
+      <div class="card-header">
+        {{$ownedevent->name}}
+      </div>
+      <div class="container text-center">
+        <div class="row justify-content-start">
+          <div class="col-4">
+            {{-- {{$ownedevent->location}} --}}
+          </div>
+          <div class="col-4">
+            {{$ownedevent->date}}
+          </div>
+          <div class="col-4">
+            <a href="{{route('Event', ['id' => $ownedevent->id])}}">info</a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  @endforeach
+    @endforeach
   @endif
 
   <div class="d-flex justify-content-center mt-4">
@@ -37,27 +36,32 @@
 <hr class="my-5">
 
 <div class="container">
+  @if ($events->isEmpty())
   <h2>Invited Events</h2>
-  @foreach ($events as $event)
-  <div class="card">
-    <div class="card-header">
-      {{$event->name}}
-    </div>
-    <div class="container text-center">
-      <div class="row justify-content-start">
-        <div class="col-4">
-          {{-- {{$event->location}} --}}
-        </div>
-        <div class="col-4">
-          {{$event->date}}
-        </div>
-        <div class="col-4">
-          <a href="{{route('Event', ['id' => $event->id])}}">info</a>
+    <p><i><b>Currently you aren't part of any events. Don't wait on friends, create your own and invite them!</b></i></p>
+  @else
+    <h2>Invited Events</h2>
+    @foreach ($events as $event)
+    <div class="card">
+      <div class="card-header">
+        {{$event->name}}
+      </div>
+      <div class="container text-center">
+        <div class="row justify-content-start">
+          <div class="col-4">
+            {{-- {{$event->location}} --}}
+          </div>
+          <div class="col-4">
+            {{$event->date}}
+          </div>
+          <div class="col-4">
+            <a href="{{route('Event', ['id' => $event->id])}}">info</a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  @endforeach
+    @endforeach
+  @endif
 
   <div class="d-flex justify-content-center mt-4">
     {!! $events->links('pagination::bootstrap-5') !!}
