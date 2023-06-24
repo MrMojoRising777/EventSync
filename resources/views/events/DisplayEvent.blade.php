de51f7e5bcf1f175d1984a9eb7fb8dcd97aa59e8
MrMojoRising777 <alexander.goyens@gmail.com>
2023-06-24 22:01:59 +0200

    DisplayEvent styling

--- resources/views/events/DisplayEvent.blade.php
+++ resources/views/events/DisplayEvent.blade.php
@@ -1,73 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
+<!-- Add ol.css manually -->
+<link rel="stylesheet" href="{{ asset('css/ol.css') }}">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
 <div class="container">
     <div class="card mb-3">
         <div class="card-header">
-           {{"Event Name: " . $event->name}}
+            <div class="header-left">
+                {{ "Event Name: " . $event->name }}
+            </div>
+            <div class="header-right">
+                {{ "Event Date: " . $event->date }}
+            </div>
         </div>
 
         <div class="container text-center">
             <div class="row justify-content-start">
                 <div class="col">
-                    {{"event date: " . $event->date}}
+                    <div class="card my-3">
+                        <div class="card-header">
+                            {{ "All Invited Friends: " }}
+                        </div>
+                        <div class="card-body">
+                            @if (!empty($friends))
+                                @foreach ($friends as $friend)
+                                    {{ $friend->username }}
+                                @endforeach
+                            @endif
+                        </div>
+                    </div>
                 </div>
                 <div class="col">
-                    {{"event location: " . $event->address . ', ' . $event->zipcode . ' ' . $event->city}}
+                    <div class="container text-center my-3">
+                        <div class="row justify-content-start">
+                            <div class="col">
+                                @include('components.mapComp')
+                            </div>
+                        </div>
+                    </div>
+                    {{ "Event Location: " . $event->address . ', ' . $event->zipcode . ' ' . $event->city }}
                 </div>
             </div>
         </div>
 
-        <hr>
-    
         <div class="container text-center">
             <div class="row justify-content-start">
                 <div class="col">
+                    {{ "Current Recommended Date: " }}
+                    @foreach ($recommended as $date)
+                        {{ $date->start_date }}
+                    @endforeach
                     @if ($event->owner_id == $user->id)
-                    <form method="POST" action="{{route('Recommended', ['id' => $event->id])}}">
+                    <form method="POST" action="{{ route('Recommended', ['id' => $event->id]) }}">
                         @csrf
                         @method('GET')
-                        <div class="form-group">
-                          <div class="col">
-                            <button type="submit" class="btn btn-success">Check recommended date</button>
-                          </div>
+                        <div class="form-group mt-3">
+                            <div class="col">
+                                <button type="submit" class="btn btn-success">Check recommended date</button>
+                            </div>
                         </div>
-                      </form>
-                    @endif
-                    {{"Current Reccomended Date: "}}
-                    @foreach ($recommended as $date)
-                        {{$date->start_date}}
-                    @endforeach
-
-                </div>
-            </div>
-        </div>
-
-
-
-        <hr>
-    
-        <div class="container text-center">
-            <div class="row justify-content-start">
-                <div class="col">
-                    {{"all invited Friends: "}}
-                    @if (!empty ($friends))
-                        @foreach ($friends as $friend)
-                            {{$friend->username}}
-                        @endforeach
+                    </form>
                     @endif
                 </div>
             </div>
         </div>
-
-        <hr>
-    
-        <div class="container text-center">
-            <div class="row justify-content-start">
-                <div class="col">
-                    {{"map"}}
-                </div>
-            </div>
-        </div>
-
+    </div>
+</div>
 @endsection
