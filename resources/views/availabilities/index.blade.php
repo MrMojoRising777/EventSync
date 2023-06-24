59b130f025f82a8eea919b25331d71ec5f6ee37e
MrMojoRising777 <alexander.goyens@gmail.com>
2023-06-24 18:13:18 +0200

    /create-event - added fancy checkboxes

--- resources/views/availabilities/index.blade.php
+++ resources/views/availabilities/index.blade.php
@@ -5,6 +5,141 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
 
+    <style>
+        .checkbox-wrapper-12 {
+            position: relative;
+        }
+
+        .checkbox-wrapper-12 > svg {
+            position: absolute;
+            top: -130%;
+            left: -170%;
+            width: 110px;
+            pointer-events: none;
+        }
+
+        .checkbox-wrapper-12 * {
+            box-sizing: border-box;
+        }
+
+        .checkbox-wrapper-12 input[type="checkbox"] {
+            -webkit-appearance: none;
+            -moz-appearance: none;
+            appearance: none;
+            -webkit-tap-highlight-color: transparent;
+            cursor: pointer;
+            margin: 0;
+        }
+
+        .checkbox-wrapper-12 input[type="checkbox"]:focus {
+            outline: 0;
+        }
+
+        .checkbox-wrapper-12 .cbx {
+            width: 24px;
+            height: 24px;
+            top: calc(100px - 12px);
+            left: calc(100px - 12px);
+        }
+
+        .checkbox-wrapper-12 .cbx input {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 24px;
+            height: 24px;
+            border: 2px solid #ffa500;
+            border-radius: 50%;
+        }
+
+        .checkbox-wrapper-12 .cbx label {
+            width: 24px;
+            height: 24px;
+            background: none;
+            border-radius: 50%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            transform: trasnlate3d(0, 0, 0);
+            pointer-events: none;
+        }
+
+        .checkbox-wrapper-12 .cbx svg {
+            position: absolute;
+            top: 5px;
+            left: 4px;
+            z-index: 1;
+            pointer-events: none;
+        }
+
+        .checkbox-wrapper-12 .cbx svg path {
+            stroke: #fff;
+            stroke-width: 3;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+            stroke-dasharray: 19;
+            stroke-dashoffset: 19;
+            transition: stroke-dashoffset 0.3s ease;
+            transition-delay: 0.2s;
+        }
+
+        .checkbox-wrapper-12 .cbx input:checked + label {
+            animation: splash-12 0.6s ease forwards;
+        }
+
+        .checkbox-wrapper-12 .cbx input:checked + label + svg path {
+            stroke-dashoffset: 0;
+        }
+
+        @-moz-keyframes splash-12 {
+            40% {
+                background: #ffa500;
+                box-shadow: 0 -18px 0 -8px #ffa500, 16px -8px 0 -8px #ffa500, 16px 8px 0 -8px #ffa500, 0 18px 0 -8px #ffa500, -16px 8px 0 -8px #ffa500, -16px -8px 0 -8px #ffa500;
+            }
+
+            100% {
+                background: #ffa500;
+                box-shadow: 0 -36px 0 -10px transparent, 32px -16px 0 -10px transparent, 32px 16px 0 -10px transparent, 0 36px 0 -10px transparent, -32px 16px 0 -10px transparent, -32px -16px 0 -10px transparent;
+            }
+        }
+
+        @-webkit-keyframes splash-12 {
+            40% {
+                background: #ffa500;
+                box-shadow: 0 -18px 0 -8px #ffa500, 16px -8px 0 -8px #ffa500, 16px 8px 0 -8px #ffa500, 0 18px 0 -8px #ffa500, -16px 8px 0 -8px #ffa500, -16px -8px 0 -8px #ffa500;
+            }
+
+            100% {
+                background: #ffa500;
+                box-shadow: 0 -36px 0 -10px transparent, 32px -16px 0 -10px transparent, 32px 16px 0 -10px transparent, 0 36px 0 -10px transparent, -32px 16px 0 -10px transparent, -32px -16px 0 -10px transparent;
+            }
+        }
+
+        @-o-keyframes splash-12 {
+            40% {
+                background: #ffa500;
+                box-shadow: 0 -18px 0 -8px #ffa500, 16px -8px 0 -8px #ffa500, 16px 8px 0 -8px #ffa500, 0 18px 0 -8px #ffa500, -16px 8px 0 -8px #ffa500, -16px -8px 0 -8px #ffa500;
+            }
+
+            100% {
+                background: #ffa500;
+                box-shadow: 0 -36px 0 -10px transparent, 32px -16px 0 -10px transparent, 32px 16px 0 -10px transparent, 0 36px 0 -10px transparent, -32px 16px 0 -10px transparent, -32px -16px 0 -10px transparent;
+            }
+        }
+
+        @keyframes splash-12 {
+            40% {
+                background: #ffa500;
+                box-shadow: 0 -18px 0 -8px #ffa500, 16px -8px 0 -8px #ffa500, 16px 8px 0 -8px #ffa500, 0 18px 0 -8px #ffa500, -16px 8px 0 -8px #ffa500, -16px -8px 0 -8px #ffa500;
+            }
+
+            100% {
+                background: #ffa500;
+                box-shadow: 0 -36px 0 -10px transparent, 32px -16px 0 -10px transparent, 32px 16px 0 -10px transparent, 0 36px 0 -10px transparent, -32px 16px 0 -10px transparent, -32px -16px 0 -10px transparent;
+            }
+        }
+    </style>
+
     <div class="card">
         <div class="card-body">
             <h1 class="card-header">Availabilities</h1>
@@ -12,10 +147,26 @@
                 <h2>Owned events:</h2>
                 <ul>
                     @foreach ($ownedEvents as $ownedevent)
-                        <li>
-                            <input type="checkbox" class="event-checkbox" value="{{ $ownedevent->id }}">
-                            <strong>Event Name:</strong> {{ $ownedevent->name }}
-                        </li>
+                    <div class="checkbox-wrapper-12">
+                        <div class="cbx">
+                            <input id="cbx-12" type="checkbox" class="event-checkbox" value="{{ $ownedevent->id }}">
+                            <label for="cbx-12"></label>
+                            <svg width="15" height="14" viewBox="0 0 15 14" fill="none">
+                            <path d="M2 8.36364L6.23077 12L13 2"></path>
+                            </svg>
+                        </div>
+                        <strong>Event Name:</strong> {{ $ownedevent->name }}    
+                        
+                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                            <defs>
+                            <filter id="goo-12">
+                                <feGaussianBlur in="SourceGraphic" stdDeviation="4" result="blur"></feGaussianBlur>
+                                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 22 -7" result="goo-12"></feColorMatrix>
+                                <feBlend in="SourceGraphic" in2="goo-12"></feBlend>
+                            </filter>
+                            </defs>
+                        </svg>
+                    </div>
                     @endforeach
                 </ul>
                 <br>
@@ -127,17 +278,16 @@
                         type: "POST",
                         data: requestData,
                         success: function (response) {
-                            console.log('Response for Event ID ' + eventId + ':', response);
+                            toastr.success('Availability added for ' + eventId);
+                            console.log('Response for Event ID ' + eventId + ':', requestData);
                             // Handle the response from the server
                         },
                         error: function (error) {
+                            toastr.log('Oops! Something went wrong.')
                             console.log('Error for Event ID ' + eventId + ':', error);
                             // Handle the error
                         }
                     });
-
-                    // Log the data being sent in the AJAX request
-                    console.log('Posted Data for Event ID ' + eventId + ':', requestData);
                 });
             });
 
