497b75c94302b1f7967b5b281118316e125d80c0
Yason <yason.pr@gmail.com>
2023-06-25 23:00:57 +0200

    added documentation for models

--- app/Models/RecommendedDate.php
+++ app/Models/RecommendedDate.php
@@ -5,12 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Model representing a recommended date for an event.
+ */
 class RecommendedDate extends Model
 {
     use HasFactory;
 
     protected $fillable = ['event_id', 'start_date', 'end_date'];
 
+    /**
+     * Get the event associated with the recommended date.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function event()
     {
         return $this->belongsTo(Event::class);
