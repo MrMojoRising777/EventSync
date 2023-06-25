ab4f5e272b58c7a9c4cba1449207438826621be6
MrMojoRising777 <alexander.goyens@gmail.com>
2023-06-25 02:29:27 +0200

    availabilities - fixed events not showing

--- app/Http/Controllers/AvailabilityController.php
+++ app/Http/Controllers/AvailabilityController.php
@@ -19,12 +19,12 @@ class AvailabilityController extends Controller
         $currentDate = Carbon::today();
 
         $events = $user->events()
-            ->whereDate('date', '>=', $currentDate)
+            // ->whereDate('date', '>=', $currentDate)
             ->orderBy('date', 'asc')
             ->paginate(5);
 
         $ownedEvents = Event::where('owner_id', '=', $user->id)
-            ->whereDate('date', '>=', $currentDate)
+            // ->whereDate('date', '>=', $currentDate)
             ->orderBy('date', 'asc')
             ->paginate(5);
 
