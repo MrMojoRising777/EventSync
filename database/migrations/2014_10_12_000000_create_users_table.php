e9761da643de7c4dae91e0d2485362f6fa93ea61
Yason <yason.pr@gmail.com>
2023-06-26 03:53:58 +0200

    merged profile picture migration into users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,7 +9,7 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
@@ -20,13 +20,14 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->string('friends');
+            $table->string('profile_picture')->nullable();
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
